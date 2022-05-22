415c87a10bb50c0e503ed4cc826a4097d4e1b74a
Morgan Kinnaman <dean.kinnaman@gmail.com>
2022-05-22 11:50:39 -0700

    added more project psuedocode

--- blog-install.sh
+++ blog-install.sh
@@ -4,8 +4,6 @@
 # Description: Simple script for setting up a debian or Ubuntu web server for static sites and emails
 # Version:     0.1
 
-# Correctly sets up 
-
 echo -e "
 _  _ _ _  _ _  _ ____ _  _ ____ _  _ . ____ 
 |_/  | |\ | |\ | |__| |\/| |__| |\ | ' [__  
@@ -18,6 +16,9 @@ _ _ _ ____ ___     ____ ____ ____ _  _ ____ ____
 _ _  _ ____ ___ ____ _    _    
 | |\ | [__   |  |__| |    |    
 | | \| ___]  |  |  | |___ |___
+
+
+
 "
 sleep 3
 
@@ -29,7 +30,7 @@ else
 fi
 
 $download update -y
-$download install -y nginx sudo certbot-nginx # necessary packages
+$download install -y nginx sudo certbot-nginx # git rsync etc
 
 # Setting text editor
 echo -e "
@@ -39,13 +40,42 @@ Which editor would you like to use?
 	3 Emacs
 	4 Nano
 
-Enter a number to select your preferred text editor: "
+Enter a number to set editor or enter to skip: "
 read EDITOR_CHOICE
-if [$EDITOR_CHOICE=="2"]; then
+if [$EDITOR_CHOICE=="1"]; then
+	$download install vim
+	# add vim as editor in bashrc
+elif[$EDITOR_CHOICE=="2"]; then
 	$download install nvim
+	# add nvim as editor in bashrc
 elif[$EDITOR_CHOICE=="3"]; then
 	$download install emacs
+	# add emacs as editor in bashrc
 elif[$EDITOR_CHOICE=="4"]; then
 	$download install nano
+	# add nano as editor in bashrc
 fi
 
+# input for basic project & site names
+# DISPLAY LINK TO DOCUMENTATION ON HOW TO SET UP DNS RECORDS FOR NEW USERS
+# Show example if they haven't
+# Ask them if they took those steps yet
+
+# Set up nginx (including port opening, firewall, etc) , download certs for sites
+
+# Add USERS and assign to SUDO group for some selected users
+# remove ROOT, password, and port 22 access. Set exclusively give access to users with keys
+# Close extra ports and remove unnecessary services
+
+# Add option for email server install & setup: first NGINX/POP3, then NGINX/IMAP/SMTP
+# ask for what emails should be made and set default/custom passwords for each 
+# set up emails of users
+# add extra verification steps & stuff to prevent emails being marked as spam on other email providers
+
+# Tell user to save information on how to connect to the server (port, syntax, etc). Assume they're a noob.
+
+# Populate guide static url on BLOG for post install information & further documentation
+
+# ADD $EDITOR_CHOICE as $EDITOR in .bashrc
+# alias repetitive nginx, systemctl, and log commands
+source ~/.bashrc
